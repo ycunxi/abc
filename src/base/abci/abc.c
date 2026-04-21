a4e2d44496e09ca56cba31caf4bbe59b85537430
Cunxi Yu <cunxiy@nvidia.com>
2026-04-21 01:48:40 -0500

    stmap8: reduce area bias

--- src/base/abci/abc.c
+++ src/base/abci/abc.c
@@ -682,6 +682,7 @@ extern int Abc_CommandStmap4                 ( Abc_Frame_t * pAbc, int argc, cha
 extern int Abc_CommandStmap5                 ( Abc_Frame_t * pAbc, int argc, char ** argv );
 extern int Abc_CommandStmap6                 ( Abc_Frame_t * pAbc, int argc, char ** argv );
 extern int Abc_CommandStmap7                 ( Abc_Frame_t * pAbc, int argc, char ** argv );
+extern int Abc_CommandStmap8                 ( Abc_Frame_t * pAbc, int argc, char ** argv );
 
 extern Aig_Man_t * Abc_NtkToDar( Abc_Ntk_t * pNtk, int fExors, int fRegisters );
 extern Abc_Ntk_t * Abc_NtkFromAigPhase( Aig_Man_t * pMan );
@@ -1159,6 +1160,7 @@ void Abc_Init( Abc_Frame_t * pAbc )
     Cmd_CommandAdd( pAbc, "SC mapping",   "stmap5",        Abc_CommandStmap5,           1 );
     Cmd_CommandAdd( pAbc, "SC mapping",   "stmap6",        Abc_CommandStmap6,           1 );
     Cmd_CommandAdd( pAbc, "SC mapping",   "stmap7",        Abc_CommandStmap7,           1 );
+    Cmd_CommandAdd( pAbc, "SC mapping",   "stmap8",        Abc_CommandStmap8,           1 );
     Cmd_CommandAdd( pAbc, "SC mapping",   "amap",          Abc_CommandAmap,             1 );
     Cmd_CommandAdd( pAbc, "SC mapping",   "phase_map",     Abc_CommandPhaseMap,         1 );
     Cmd_CommandAdd( pAbc, "SC mapping",   "stochmap",      Abc_CommandStochMap,         1 );
